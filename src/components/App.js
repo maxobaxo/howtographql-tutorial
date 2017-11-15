3fed9f22da848411abb475f5f20407ef1b4c2eb7
Max Scher <maxobaxo@gmail.com>
2017-11-14 20:51:59 -0800

    created simple graphql queries, passing response data as props to viewed component

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+import LinkList from './LinkList'
+
+class App extends Component {
+  render() {
+    return (
+      <LinkList />
+    );
+  }
+}
+
+export default App;
