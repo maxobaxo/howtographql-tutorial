ecbf7cbe2624fdcf5536b42f198f77cbd382e897
Max Scher <maxobaxo@gmail.com>
2017-11-15 09:53:58 -0800

    add basic mutation

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,15 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 // import './App.css';
 import LinkList from './LinkList'
+import CreateLink from './CreateLink'
 
 class App extends Component {
   render() {
     return (
-      <LinkList />
+      <div>
+        <CreateLink />
+        <LinkList />
+      </div>
     );
   }
 }
